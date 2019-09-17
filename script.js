8c1618bfce1a5590facabdc7bd9e526e4511ec1c
Esin Saribudak <esin@Esins-Air.attlocal.net>
2019-09-17 14:26:50 -0500

    answer choice click event listener and feedback on correct or not

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
+//obtain engage button from DOM
 const engageButton = document.getElementById("engage");
 
+//engage button begins quiz and switches to next question
 engageButton.addEventListener("click", () => {
     console.log("You clicked engage!");
-    changeQuestion()})
+    changeQuestion();
+})
 
+//obtain questions from DOM
 const questionArray = document.getElementsByClassName("question");
 
+//question incrementor
 let currentQuestion = 0;
 
+
+//change question button
 function changeQuestion() {
     if (currentQuestion < questionArray.length){
         questionArray[currentQuestion].style.display = "block";
@@ -15,12 +22,27 @@ function changeQuestion() {
         {
         questionArray[currentQuestion-1].style.display = "none";
         }
+        checkAnswer();
         
     }
     currentQuestion++;
+    
     if (currentQuestion > questionArray.length) {
         console.log("Reached end of questions.")
         questionArray[questionArray.length-1].style.display = "none";
         document.getElementById("end-page").style.display = "block";
     }
+}
+
+//check if answer is correct
+function checkAnswer() {
+    let answerChoices = document.querySelectorAll("ol")[currentQuestion];
+    answerChoices.addEventListener("click", (evt) => {
+        if (evt.target.classList.contains("correct")) {
+            alert("Correct!")
+        }
+        else {
+            alert("Incorrect.")
+        }
+    })
 }
