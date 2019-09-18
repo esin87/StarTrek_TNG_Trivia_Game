2657a479bf4704390a24da3bd8bf3ee352080319
Esin Saribudak <esin@Esins-Air.attlocal.net>
2019-09-18 15:44:13 -0500

    worked out bug that skipped questions

--- script.js
+++ script.js
@@ -16,11 +16,17 @@ function increaseScore() {
 
 //engage button begins quiz and switches to next question
 engageButton.addEventListener("click", () => {
-    console.log("You clicked engage!");
+    //change score display to appear
     scoreDisplay.style.display = "block";
-    changeQuestion();
+    //if game hasn't started, use Engage to start game
+    if (currentQuestion===0) {
+        changeQuestion();
+    }
     
+    //hide intro page
     document.getElementById("intro").style.display = "none";
+
+    //if game is over, use engage to restart 
     if (currentQuestion > 10) {
         document.location.reload();
     }
@@ -35,62 +41,86 @@ let currentQuestion = 0;
 
 //change question button
 function changeQuestion() {
-    engageButton.style.display="none"
-    if (currentQuestion < questionArray.length){
-        questionArray[currentQuestion].style.display = "block";
-        if (currentQuestion > 0)
-        {
-        questionArray[currentQuestion-1].style.display = "none";
-        }
-        checkAnswer();
-        
+    //hide engage button
+    if (engageButton.style.display!=="none") {
+        engageButton.style.display="none"
     }
-    currentQuestion++;
+    //if current question is less than question array length
+    if (currentQuestion < questionArray.length){
+        //show current question
+        questionArray[currentQuestion].style.display = "block";
+        //if past the first question, hide the previous question
+        if (currentQuestion > 0){
+            questionArray[currentQuestion-1].style.display = "none";
+            }
+        //run check answer function
+        checkAnswer();
+       
+    }
     
+    //increment current question
+    currentQuestion++;
+
+    //if end of questions is reached
     if (currentQuestion > questionArray.length) {
+        //show engage button again
         engageButton.style.display="block"
-        console.log("Reached end of questions.")
+        //hide last question
         questionArray[questionArray.length-1].style.display = "none";
+        //show score
         document.getElementById("end-page").innerText = `You scored ${score} out of 10! Press "ENGAGE!" to play again.`
+        //show end page div
         document.getElementById("end-page").style.display = "block";
     }
+     
 }
-
+//grab answer feedback div from dom
 let answerFeedbackDiv = document.querySelector("#answer-feedback");
+//grab answer feedback text div from dom
 let answerFeedbackText = document.querySelector("#answer-feedback-text")
+
 //check if answer is correct
 function checkAnswer() {
+    //grab ordered list from dom
     let answerChoices = document.querySelectorAll("ol")[currentQuestion];
+    //add event listener to entire ordered list object
     answerChoices.addEventListener("click", (evt) => {
+        //if class is correct, show correct feedback and increase score
         if (evt.target.classList.contains("correct")) {
             showCorrectAnswerFeedback();
             increaseScore();
         }
+        //else show incorrect answer
         else {
             showIncorrectAnswerFeedback();
         }
+        changeQuestion();
     })
 }
 
 function showCorrectAnswerFeedback() {
+    //add feedback to text div
     answerFeedbackText.innerText = "Correct! Click anywhere to continue."
+    //make feedback div overlay visible
     answerFeedbackDiv.style.display = "block"
-    answerFeedbackDiv.addEventListener("click", () => {
-        hideAnswerFeedback();
-        changeQuestion();
-    })
+    //hide feedback and continue game
+    hideAnswerFeedback();
 }
 
 function showIncorrectAnswerFeedback() {
+    //add feedback to text div
     answerFeedbackText.innerText = "Incorrect. Click anywhere to continue.";
+    //make feedback div overlay visible
     answerFeedbackDiv.style.display = "block";
-    answerFeedbackDiv.addEventListener("click", () => {
-        hideAnswerFeedback();
-        changeQuestion();
-    })
+    //hide feedback and continue game
+    hideAnswerFeedback();
+    
 }
 
 function hideAnswerFeedback() {
-    answerFeedbackDiv.style.display="none";
-    answerFeedbackText.innerText = ""
+    answerFeedbackDiv.addEventListener("click", () => {
+        answerFeedbackDiv.style.display="none";
+        answerFeedbackText.innerText = "";
+        
+    })
 }
